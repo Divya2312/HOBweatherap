4110c14670a50e1843e9813a22e9c871ffe3b6d5
Divya2312 <81798803+Divya2312@users.noreply.github.com>
2022-06-06 14:02:05 +0530

    task1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
 function App() {
+
+  const datecreater=(d)=>{
+    let months=["janaury","Februray","March","April","May","June","July","Agust","September","October",
+                "November","December"];
+    let days=  ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"];
+    
+    let day=days[d.getDay()];
+    let date=d.getDate();
+    let month=months[d.getMonth()];
+    let year=d.getFullYear();
+    let time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    return `${day} ${date} ${month} ${year}  ${time}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" 
+          className="search-bar" 
+          placeholder="search here"/>
+        </div>
+        <div className="location-box">
+          <div className="location">Mumbai ,INDIA</div>
+          <div className="date">{datecreater(new Date())}</div>
+       </div>
+       <div className="weather-box">
+         <div className="temp">
+           20 ° C
+         </div>
+         <div className="weather">Sunny</div>
+
+       </div>
+      </main>
+    
     </div>
   );
 }
